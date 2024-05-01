de552442d07af6ef8d7521a153089641ff12feec
Vladyslav Kozin <vladiksystem@gmail.com>
2024-05-01 23:43:31 +0300

    chore: change config

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,17 +13,19 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     baseURL: testConfig.baseURL,
-
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
     headless: process.env.CI ? true : false,
   },
-
   /* Configure projects for major browsers */
   projects: [
     {
       name: "chromium",
-      use: { ...devices["Desktop Chrome"], screenshot: "only-on-failure" },
+      use: {
+        ...devices["Desktop Chrome"],
+        screenshot: "only-on-failure",
+        video: 'retain-on-failure',
+      },
     },
 
     // {
