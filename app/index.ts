629aeb72e4b1122e5f6ca95842f53397dc2bd762
Vladyslav Kozin <36770161+kozinvl@users.noreply.github.com>
2024-05-06 14:15:25 +0300

    Add doc strings (#11)
    * chore: add doc strings
    
    * chore: remove line

--- app/index.ts
+++ app/index.ts
@@ -4,6 +4,11 @@ import { testConfig } from "../testConfig";
 
 export class Application extends AuthController {
 
+  /**
+   * A function that performs a headless login using the provided testConfig email and password.
+   *
+   * @return {Promise<void>} A promise that resolves when the login is successful.
+   */
   async headlessLogin() {
     await this.login({
       email: testConfig.email,
@@ -11,6 +16,12 @@ export class Application extends AuthController {
     });
   }
 
+  /**
+   * Sets a cookie in the browser context using the provided page object.
+   *
+   * @param {Page} page - The page object representing the browser context.
+   * @return {Promise<void>} A promise that resolves when the cookie is set.
+   */
   async setCookieLogin(page: Page) {
     await page.evaluate((cookie: string) => {
       document.cookie = cookie;
