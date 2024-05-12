727b4726347402f6ec8b406f311132b67222319c
Vladyslav Kozin <vladiksystem@gmail.com>
2024-05-12 21:14:35 +0300

    test: add tsconfig path

---
+++ app/pages/index.ts
@@ -0,0 +1,5 @@
+import { MainPage } from './main.page'
+import { DashboardPage } from './dashboard.page'
+import { FormAuthenticationPage } from './login.page'
+
+export { MainPage, DashboardPage, FormAuthenticationPage }
